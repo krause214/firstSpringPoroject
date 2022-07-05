8f1bc623c6235d10091a80db70e0432ab6a68c83
Andrey <andron-2102@mail.ru>
2022-07-05 15:37:22 +0400

    gitInit

---
+++ src/test/java/my/babicev/firstSpringPoroject/FirstSpringPorojectApplicationTests.java
@@ -0,0 +1,13 @@
+package my.babicev.firstSpringPoroject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringPorojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
